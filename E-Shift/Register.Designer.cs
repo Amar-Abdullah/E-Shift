fd7046073810cfd00580f4434c60834510a7bd38
Amar-Abdullah <amarabdullah5148@gmail.com>
2025-07-31 16:03:57 +0530

    Add Create New Account button and adjust layout
    - Introduced a new button `Create_New_Account_button` for user account creation with specified properties.
    - Adjusted positions of `Create_Your_Account` and `Slogan_label` for better alignment.
    - Updated locations and sizes of several labels and text boxes to maintain layout consistency.
    - Increased form `ClientSize` to accommodate the new button and adjusted layout.
    - Ensured the new button is included in the form's controls for rendering.

--- E-Shift/Register.Designer.cs
+++ E-Shift/Register.Designer.cs
@@ -53,6 +53,7 @@
             this.textBox8 = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
             this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.Create_New_Account_button = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -60,7 +61,7 @@
             // 
             this.Create_Your_Account.AutoSize = true;
             this.Create_Your_Account.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Create_Your_Account.Location = new System.Drawing.Point(886, 106);
+            this.Create_Your_Account.Location = new System.Drawing.Point(965, 106);
             this.Create_Your_Account.Name = "Create_Your_Account";
             this.Create_Your_Account.Size = new System.Drawing.Size(311, 36);
             this.Create_Your_Account.TabIndex = 0;
@@ -80,7 +81,7 @@
             // 
             this.Slogan_label.AutoSize = true;
             this.Slogan_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Slogan_label.Location = new System.Drawing.Point(913, 153);
+            this.Slogan_label.Location = new System.Drawing.Point(992, 153);
             this.Slogan_label.Name = "Slogan_label";
             this.Slogan_label.Size = new System.Drawing.Size(268, 18);
             this.Slogan_label.TabIndex = 2;
@@ -90,9 +91,9 @@
             // 
             this.First_Name_label.AutoSize = true;
             this.First_Name_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.First_Name_label.Location = new System.Drawing.Point(732, 227);
+            this.First_Name_label.Location = new System.Drawing.Point(811, 227);
             this.First_Name_label.Name = "First_Name_label";
-            this.First_Name_label.Size = new System.Drawing.Size(111, 24);
+            this.First_Name_label.Size = new System.Drawing.Size(107, 22);
             this.First_Name_label.TabIndex = 3;
             this.First_Name_label.Text = "First Name";
             // 
@@ -100,9 +101,9 @@
             // 
             this.Last_Name_label.AutoSize = true;
             this.Last_Name_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Last_Name_label.Location = new System.Drawing.Point(733, 292);
+            this.Last_Name_label.Location = new System.Drawing.Point(812, 292);
             this.Last_Name_label.Name = "Last_Name_label";
-            this.Last_Name_label.Size = new System.Drawing.Size(108, 24);
+            this.Last_Name_label.Size = new System.Drawing.Size(105, 22);
             this.Last_Name_label.TabIndex = 4;
             this.Last_Name_label.Text = "Last Name";
             // 
@@ -110,9 +111,9 @@
             // 
             this.Address_Information_label.AutoSize = true;
             this.Address_Information_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Address_Information_label.Location = new System.Drawing.Point(733, 367);
+            this.Address_Information_label.Location = new System.Drawing.Point(812, 367);
             this.Address_Information_label.Name = "Address_Information_label";
-            this.Address_Information_label.Size = new System.Drawing.Size(87, 24);
+            this.Address_Information_label.Size = new System.Drawing.Size(83, 22);
             this.Address_Information_label.TabIndex = 5;
             this.Address_Information_label.Text = "Address";
             // 
@@ -120,9 +121,9 @@
             // 
             this.Email_label.AutoSize = true;
             this.Email_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Email_label.Location = new System.Drawing.Point(733, 552);
+            this.Email_label.Location = new System.Drawing.Point(812, 552);
             this.Email_label.Name = "Email_label";
-            this.Email_label.Size = new System.Drawing.Size(62, 24);
+            this.Email_label.Size = new System.Drawing.Size(59, 22);
             this.Email_label.TabIndex = 6;
             this.Email_label.Text = "Email";
             // 
@@ -130,9 +131,9 @@
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(732, 617);
+            this.label6.Location = new System.Drawing.Point(811, 617);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(152, 24);
+            this.label6.Size = new System.Drawing.Size(142, 22);
             this.label6.TabIndex = 7;
             this.label6.Text = "Phone Number";
             // 
@@ -140,9 +141,9 @@
             // 
             this.Create_New_Password_label.AutoSize = true;
             this.Create_New_Password_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Create_New_Password_label.Location = new System.Drawing.Point(732, 678);
+            this.Create_New_Password_label.Location = new System.Drawing.Point(811, 678);
             this.Create_New_Password_label.Name = "Create_New_Password_label";
-            this.Create_New_Password_label.Size = new System.Drawing.Size(215, 24);
+            this.Create_New_Password_label.Size = new System.Drawing.Size(208, 22);
             this.Create_New_Password_label.TabIndex = 8;
             this.Create_New_Password_label.Text = "Create New Password";
             // 
@@ -150,16 +151,16 @@
             // 
             this.Confirm_Password_label.AutoSize = true;
             this.Confirm_Password_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Confirm_Password_label.Location = new System.Drawing.Point(733, 742);
+            this.Confirm_Password_label.Location = new System.Drawing.Point(812, 742);
             this.Confirm_Password_label.Name = "Confirm_Password_label";
-            this.Confirm_Password_label.Size = new System.Drawing.Size(178, 24);
+            this.Confirm_Password_label.Size = new System.Drawing.Size(172, 22);
             this.Confirm_Password_label.TabIndex = 9;
             this.Confirm_Password_label.Text = "Confirm Password";
             // 
             // textBox1
             // 
             this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.textBox1.Location = new System.Drawing.Point(958, 219);
+            this.textBox1.Location = new System.Drawing.Point(1037, 219);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(239, 32);
@@ -168,7 +169,7 @@
             // textBox2
             // 
             this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.textBox2.Location = new System.Drawing.Point(958, 284);
+            this.textBox2.Location = new System.Drawing.Point(1037, 284);
             this.textBox2.Multiline = true;
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(239, 32);
@@ -177,7 +178,7 @@
             // textBox3
             // 
             this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.textBox3.Location = new System.Drawing.Point(958, 357);
+            this.textBox3.Location = new System.Drawing.Point(1037, 357);
             this.textBox3.Multiline = true;
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(239, 32);
@@ -186,7 +187,7 @@
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(1394, 361);
+            this.comboBox1.Location = new System.Drawing.Point(1473, 361);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(171, 24);
             this.comboBox1.TabIndex = 13;
@@ -195,9 +196,9 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(1232, 359);
+            this.label1.Location = new System.Drawing.Point(1311, 359);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(44, 24);
+            this.label1.Size = new System.Drawing.Size(45, 22);
             this.label1.TabIndex = 14;
             this.label1.Text = "City";
             // 
@@ -205,16 +206,16 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(732, 432);
+            this.label2.Location = new System.Drawing.Point(811, 432);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(158, 24);
+            this.label2.Size = new System.Drawing.Size(153, 22);
             this.label2.TabIndex = 15;
             this.label2.Text = "Postal/Zip Code";
             // 
             // textBox4
             // 
             this.textBox4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.textBox4.Location = new System.Drawing.Point(958, 421);
+            this.textBox4.Location = new System.Drawing.Point(1037, 421);
             this.textBox4.Multiline = true;
             this.textBox4.Name = "textBox4";
             this.textBox4.Size = new System.Drawing.Size(239, 32);
@@ -224,9 +225,9 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(1232, 429);
+            this.label3.Location = new System.Drawing.Point(1311, 429);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(144, 24);
+            this.label3.Size = new System.Drawing.Size(141, 22);
             this.label3.TabIndex = 17;
             this.label3.Text = "State/Province";
             // 
@@ -234,7 +235,7 @@
             // 
             this.comboBox2.FormattingEnabled = true;
             this.comboBox2.ItemHeight = 16;
-            this.comboBox2.Location = new System.Drawing.Point(1394, 429);
+            this.comboBox2.Location = new System.Drawing.Point(1473, 429);
             this.comboBox2.Name = "comboBox2";
             this.comboBox2.Size = new System.Drawing.Size(201, 24);
             this.comboBox2.TabIndex = 18;
@@ -242,7 +243,7 @@
             // textBox5
             // 
             this.textBox5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.textBox5.Location = new System.Drawing.Point(958, 541);
+            this.textBox5.Location = new System.Drawing.Point(1037, 541);
             this.textBox5.Multiline = true;
             this.textBox5.Name = "textBox5";
             this.textBox5.Size = new System.Drawing.Size(239, 32);
@@ -251,7 +252,7 @@
             // textBox6
             // 
             this.textBox6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.textBox6.Location = new System.Drawing.Point(958, 607);
+            this.textBox6.Location = new System.Drawing.Point(1037, 607);
             this.textBox6.Multiline = true;
             this.textBox6.Name = "textBox6";
             this.textBox6.Size = new System.Drawing.Size(239, 32);
@@ -260,7 +261,7 @@
             // textBox7
             // 
             this.textBox7.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.textBox7.Location = new System.Drawing.Point(958, 670);
+            this.textBox7.Location = new System.Drawing.Point(1037, 670);
             this.textBox7.Multiline = true;
             this.textBox7.Name = "textBox7";
             this.textBox7.Size = new System.Drawing.Size(239, 32);
@@ -269,7 +270,7 @@
             // textBox8
             // 
             this.textBox8.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.textBox8.Location = new System.Drawing.Point(958, 734);
+            this.textBox8.Location = new System.Drawing.Point(1037, 734);
             this.textBox8.Multiline = true;
             this.textBox8.Name = "textBox8";
             this.textBox8.Size = new System.Drawing.Size(239, 32);
@@ -279,25 +280,37 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(733, 491);
+            this.label4.Location = new System.Drawing.Point(812, 491);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(80, 24);
+            this.label4.Size = new System.Drawing.Size(76, 22);
             this.label4.TabIndex = 23;
             this.label4.Text = "Gender";
             // 
             // comboBox3
             // 
             this.comboBox3.FormattingEnabled = true;
-            this.comboBox3.Location = new System.Drawing.Point(958, 489);
+            this.comboBox3.Location = new System.Drawing.Point(1037, 489);
             this.comboBox3.Name = "comboBox3";
             this.comboBox3.Size = new System.Drawing.Size(127, 24);
             this.comboBox3.TabIndex = 24;
+            // 
+            // Create_New_Account_button
+            // 
+            this.Create_New_Account_button.BackColor = System.Drawing.SystemColors.Highlight;
+            this.Create_New_Account_button.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Create_New_Account_button.Location = new System.Drawing.Point(953, 836);
+            this.Create_New_Account_button.Name = "Create_New_Account_button";
+            this.Create_New_Account_button.Size = new System.Drawing.Size(274, 52);
+            this.Create_New_Account_button.TabIndex = 25;
+            this.Create_New_Account_button.Text = "Create New Account";
+            this.Create_New_Account_button.UseVisualStyleBackColor = false;
             // 
             // Register
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1558, 841);
+            this.ClientSize = new System.Drawing.Size(1676, 931);
+            this.Controls.Add(this.Create_New_Account_button);
             this.Controls.Add(this.comboBox3);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.textBox8);
@@ -359,5 +372,6 @@
         private System.Windows.Forms.TextBox textBox8;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.Button Create_New_Account_button;
     }
 }
